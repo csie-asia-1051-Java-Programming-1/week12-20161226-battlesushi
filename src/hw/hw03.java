d0725efd598773d0fb86a533995cf65186d3c174
battlesushi <105021030@live.asia.edu.tw>
2017-01-01 20:17:22 +0800

    hw

--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -1,9 +1,8 @@
 package hw;
-
+import java.util.Scanner;
 public class hw03 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
 
 	}
 
