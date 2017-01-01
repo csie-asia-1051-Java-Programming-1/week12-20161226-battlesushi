7002278f5454f61a2a16ec535822d0b97fede315
battlesushi <105021030@live.asia.edu.tw>
2017-01-01 21:06:58 +0800

    hw

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,7 +1,7 @@
 package hw;
-
+import java.util.Scanner;
 public class hw02 {
-
+	//做法-->  寫個[n][2] 陣列   把輸入的數[n][0]及對應數寫好[n][1]  再用charAt[n][0]找  輸出其對應值[n][1]
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/hw/hw03.java
+++ src/hw/hw03.java
@@ -3,7 +3,24 @@ import java.util.Scanner;
 public class hw03 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-
+		String str=sc.nextLine();
+		String str2=str.toLowerCase();
+		String data[]=str2.split(" ");
+		for(int i=0;i<data.length-1;i++){
+			for(int j=i+1;j<data.length;j++){
+				if(data[i].equals(data[j]))
+					data[j]="";
+			}
+		}
+		for(int i=0;i<data.length;i++){
+			if(i==data.length-1)
+				System.out.print(data[i]);
+			else
+				System.out.print(data[i]+" ");
+		}
+		
+		
+		
 	}
 
 }
